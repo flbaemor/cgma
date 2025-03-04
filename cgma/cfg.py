050971b0e6b2dd06e2d78f70fd118b54dd551f88
r1moo <rlsdeldoc@gmail.com>
2025-03-04 15:10:19 +0800

    1

--- cgma/cfg.py
+++ cgma/cfg.py
@@ -104,7 +104,7 @@ cfg = {
     "<program>": [["<start>"]],
     "<start>":[["<global_declaration>", "<start>"],["chungus", "<chungus_follow>"]],
     "<chungus_follow>":[["<identifier>", "<declaration_tail>", "<start>"], ["skibidi", "(", ")", "{", "<body_main>", "back", "0", "}"]],
-    "<global_declaration>": [["ε"], ["<constant_var>"], ["<gldata_type>", "<identifier>", "<declaration_tail>"], ["<expression>"]],
+    "<global_declaration>": [["<constant_var>"], ["<gldata_type>", "<identifier>", "<declaration_tail>"], ["<expression>"]],
     "<gldata_type>": [["aura"], ["gng"], ["nocap"], ["forsen"], ["forsencd"], ["chudeluxe"], ["lwk"]],
     "<data_type>": [["chungus"], ["<gldata_type>"]],
     "<expression>": [["<operand>", "<expression_tail>"]],
